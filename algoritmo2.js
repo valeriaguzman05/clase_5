aae507006c3997296809ac98ad755d4beb94ca5f
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-09-06 21:35:16 -0500

    first batch

---
+++ algoritmo2.js
@@ -0,0 +1,12 @@
+    const nivel = parseFloat("15")
+
+    if (nivel>20) {
+        console.log("Eres un ninja")
+    } else {
+        if (nivel>=10) {
+            console.log("Eres acrobata")
+        } else {
+            console.log("Eres aprendiz")
+        }        
+    }
+    
---
+++ hello_world.js
@@ -0,0 +1,1 @@
+console.log("Pues ahi va");
